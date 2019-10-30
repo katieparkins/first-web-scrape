e05194f6ff46c0e14350cc20aab98285589d8627
Katie Parkins <katieparkins1@MacBook-Pro-8.domain>
2019-10-29 19:52:11 -0500

    first commit

---
+++ first-web-scrape.py
@@ -0,0 +1,5 @@
+###importing libraries that make it possible to scrape website###
+import requests, mechanize
+from bs$ import Beautiful Soup
+###setting up whole url as string so you don't have to type full url everytime###
+url = 'https://www.mshp.dps.missouri.gov/HP68/SearchAction'
